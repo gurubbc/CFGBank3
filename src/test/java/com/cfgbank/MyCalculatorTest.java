66338d2273830bfdd5429fbd83263b90838fea6a
Guru <gurumurthy.ramamurthy@gmail.com>
2024-03-29 11:26:48 +0100

    maven project with junit and jacoco

---
+++ src/test/java/com/cfgbank/MyCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.cfgbank;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MyCalculatorTest {
+	@Test
+	public void testAdd() {
+		MyCalculator mc=new MyCalculator();
+		int actual=mc.add(5, 5);
+		Assert.assertEquals(10, actual);
+		
+	}
+}
